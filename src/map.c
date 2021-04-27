5d1cd4de9d9881909d2a3cc88b921328675db13c
nsklaus <nsklaus@yandex.com>
2021-04-27 22:53:28 +0200

    extract multiple needed lines from map file

--- src/map.c
+++ src/map.c
@@ -7,19 +7,26 @@ void loadmap() {
 
     /* WIP: open map file, read line by line, search for patterns 
     if found store it, in array, later on process the array to construct game map */
-    static const char filename[] = "assets/level01.tmx";
+
+    char *patterns[10] = { "map version", "tileset firstgid", // pattern to search for
+                           "image source", "layer id" };  
+                           //, "data encoding" };
+
+    static const char filename[] = "assets/level01.tmx"; // map file
     FILE *file = fopen(filename, "r");
     int count = 0;
     if ( file != NULL ) 
     {
         char line[256]; // maximum line size
-        const char *mystring = "layer id"; // pattern to search for
+        //const char *mystring = "layer id"; 
         while (fgets(line, sizeof line, file) != NULL) // read line by line 
         {
-            if (strstr(line,mystring)) // if pattern is found
-            {
-                printf("%s\n",line); // print it
-                fflush(stdout); 
+            for (int i = 0; i < 9; i++ ) { // check each line with each entry of patterns array
+                if (patterns[i] != NULL && strstr(line,patterns[i])) // if pattern is found
+                {
+                    printf("%s\n",line); // print it
+                    fflush(stdout); 
+                }
             }
         }
         fclose(file);
