109b1350ca5cd997535cf7da1b2187de5bf77b25
nsklaus <nsklaus@yandex.com>
2021-04-27 22:13:06 +0200

    search for pattern in map file, if found print them on stdout

--- src/map.c
+++ src/map.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "map.h"
 
 void loadmap() {
-  FILE *fp;
 
-  if (( fp = fopen("assets/level01.tmx","r")) == NULL) {
-    printf("bleh");
-  }
-  int ch;
-  while((ch = fgetc(fp)) != EOF) {
-    putchar(ch);
-  }
-
-  fflush(stdout); 
-  fclose(fp); 
+    /* WIP: open map file, read line by line, search for patterns 
+    if found store it, in array, later on process the array to construct game map */
+    static const char filename[] = "assets/level01.tmx";
+    FILE *file = fopen(filename, "r");
+    int count = 0;
+    if ( file != NULL ) 
+    {
+        char line[256]; // maximum line size
+        const char *mystring = "layer id"; // pattern to search for
+        while (fgets(line, sizeof line, file) != NULL) // read line by line 
+        {
+            if (strstr(line,mystring)) // if pattern is found
+            {
+                printf("%s\n",line); // print it
+                fflush(stdout); 
+            }
+        }
+        fclose(file);
+    } 
+    else 
+    {
+        printf("file doesn't exist\n");
+        fflush(stdout); 
+    }
 }
