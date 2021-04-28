2dad850ca7d68ed86c0f585d86c74acc5b9d886a
nsklaus <nsklaus@yandex.com>
2021-04-28 22:57:47 +0200

    testing jsmn json parser

--- src/map.c
+++ src/map.c
@@ -1,34 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <json-c/json.h>
+//#include <json-c/json.h>
+#include "jsmn.h"
 #include "map.h"
 
 void loadmap() {
 
+    /* test jsmn json parser */
+    jsmn_parser parser;
+    jsmntok_t tokens[10];
+
+    jsmn_init(&parser);
+
+
     /* WIP: open map file, read line by line, search for patterns 
     if found store it, in array, later on process the array to construct game map */
-
     char *patterns[10] = { "map version", "tileset firstgid", // pattern to search for
                            "image source", "layer id" };  
                            //, "data encoding" };
 
-    static const char filename[] = "assets/level01.tmx"; // map file
+    static const char filename[] = "assets/level01.json"; // map file
     FILE *file = fopen(filename, "r");
+
     int count = 0;
     if ( file != NULL ) 
     {
         char line[256]; // maximum line size
+        char *p = 0; 
+        int len = 0; 
         while (fgets(line, sizeof line, file) != NULL) // read line by line 
         {
-            for (int i = 0; i < 9; i++ ) { // check each line with each entry of patterns array
-                if (patterns[i] != NULL && strstr(line,patterns[i])) // if pattern is found
-                {
-                    printf("%s\n",line); // print it
-                    fflush(stdout); 
-                }
-            }
-        }
+            int llen = strlen(line);  // get length of 'line'
+            p = realloc(p, len + llen + 1);  // ?
+            memcpy(p+len, line, llen);   // ?
+            len += llen;   // ?
+        } 
+        printf("%s \n", p); 
+        jsmn_parse(&parser, p, strlen(p), tokens, 10);
+
+        p[len] = 0;
         fclose(file);
     } 
     else 
