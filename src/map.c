c7a8eb5992020737af0e1bd69885630c63b8a274
nsklaus <nsklaus@yandex.com>
2021-05-08 21:37:55 +0200

    2d array for layer data[2][3200]

--- src/map.c
+++ src/map.c
@@ -17,20 +17,11 @@ struct mymap {
     int map_width;
     int map_height;
     int layer_name;
-    int *layer_data; 
+    int *layer_data[3]; 
     char *tileset_name;
     int tileset_firstgid;
     int counter;
 };
-
-// int is_leaf(xmlNode * node){
-//     xmlNode * child = node->children;
-//     while(child){
-//         if(child->type == XML_ELEMENT_NODE) return 0;
-//         child = child->next;
-//     }
-//     return 1;
-// }
 
 static void print_element_names(xmlNode * a_node, struct mymap *map)
 {
@@ -46,25 +37,46 @@ static void print_element_names(xmlNode * a_node, struct mymap *map)
 
         char *map_info = "map";
         if ( strcmp(name,map_info) == 0){
-            printf("(char)name = %s\n", xmlGetProp(cur_node, (const xmlChar *)"width")); //debug output "80" (char)
-            printf("(char)name = %s\n", xmlGetProp(cur_node, (const xmlChar *)"height")); //debug output "40" (char)
-            map->map_width = (int)atoi((char *)xmlGetProp(cur_node, (const xmlChar *)"width")); // get width
-            map->map_height = (int)atoi((char *)xmlGetProp(cur_node, (const xmlChar *)"height")); // get height
-            printf("(int)map.map_width = %d\n", map->map_width ); //debug output int 80 (int)
-            printf("(int)map.map_height = %d\n", map->map_height ); //debug output int 40 (int)
+            map->map_width = atoi((char *)xmlGetProp(cur_node, (const xmlChar *)"width")); // get width
+            map->map_height = atoi((char *)xmlGetProp(cur_node, (const xmlChar *)"height")); // get height
         }
 
         char* data = "data";
         if ( strcmp(name,data) == 0){
+
+            int layer_data_length = map->map_width * map->map_height * 4; // works
+            map->layer_data[0] = malloc(layer_data_length); // works
+
             printf(" --name = %s, \n", cur_node->name);
             //printf(" content = %s \n", cur_node->children->content) // commented out to reduce output on term
+            char *string = (char *)cur_node->children->content; 
+            char *pt = string;
+            int counter = 0;
+            int layer_number = 0;
+            pt = strtok (string,",");
+            while (pt != NULL) {
+                int a = atoi(pt);
+                if (counter == 3200)
+                    layer_number ++;
+                if (counter == 6400)
+                    layer_number ++;   
+                map->layer_data[layer_number][counter] = a;
+                printf("map->layer_data[counter]=%d\n",map->layer_data[layer_number][counter]);
+                //printf("%d\n", a);
+                pt = strtok (NULL, ",");
+                counter++;
+                if (counter<3200)
+                    map->counter ++;
+            }
             //int layer_data_length = map->map_width * map->map_height * 4; // works
             //map->layer_data = malloc(layer_data_length); // works
-            int layer_data_length = 60; // use shorter value than above for debug purpose
-            for (int i = 0; i < layer_data_length/4; i++) {
-                map->layer_data[i] = 111; // assign bogus debug value
-                map->counter = i; // keep track of map.layer_data length
-            }
+            //int layer_data_length = 60; // use shorter value than above for debug purpose
+
+            //worked without malloc
+            // for (int i = 0; i < layer_data_length/4; i++) {
+            //     map->layer_data[i] = 111; // assign bogus debug value
+            //     map->counter = i; // keep track of map.layer_data length
+            // }
         }
         print_element_names( cur_node->children, map );
     }
@@ -72,7 +84,8 @@ static void print_element_names(xmlNode * a_node, struct mymap *map)
 
 void loadmap() {
 
-    struct mymap map; // instanciate struct
+    //struct mymap map;
+    struct mymap map = {0};
     xmlDoc *doc = NULL;
     xmlNode *root_element = NULL;
 
@@ -82,13 +95,16 @@ void loadmap() {
     print_element_names(root_element, &map);
     xmlFreeDoc(doc);
     xmlCleanupParser();
-
+    int tt = 0;
     for (int i = 0; i < map.counter; i++) {
         // verify layer_data[0-14] = 111
-        printf("****layer_data = %d \n", map.layer_data[i]);
+        if (map.layer_data[0][i] != 0) {
+            printf("****layer_data = %d, i = %d \n", map.layer_data[0][i], i);
+            tt ++;
+        }
     }
-    printf("-----map.counter=%d\n",map.counter); //debug: 60/4 = 15
-    
+    printf("+++++layer_data_length = %d \n", map.counter);  // 3200 * 3 (3 layers.. 80*40)
+
     static const char filename[] = "assets/level01.tmx"; // map file
     FILE *file = fopen(filename, "r"); 
 
